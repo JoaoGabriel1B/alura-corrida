a9ae05b3abc400688ad34041b085890fe0201509
JoaoGabriel1B <00001095104251sp@al.educacao.sp.gov.br>
2025-03-31 18:39:13 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,51 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+let xJogador1 = 0;
+let xJogador2 = 0;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#FF0000");
+  } else {
+    background("#0007FF");
+  }
+}
+
+function desenhaJogadores() {
+  textSize(40);
+  text("🚗", xJogador1, 100);
+  text("🚙", xJogador2, 300);
+}
+
+function desenhaLinhaDeChegada() {
+  rect(350, 0, 10, 400);
+}
+
+function verificaVencedor() {
+  if (xJogador1 > 350) {
+    text("Jogador 1 venceu!", 50, 200);
+    noLoop();
+  }
+  if (xJogador2 > 350) {
+    text("Jogador 2 venceu!", 50, 200);
+    noLoop();
+  }
+}
+
+function keyReleased() {
+  if (key == "a") {
+    xJogador1 += random(20);
+  }
+  if (key == "s") {
+    xJogador2 += random(20);
+  }
+}
